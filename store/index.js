02ca5b2d8564741094f9ff075278d3198aa8ea04
ahmedallani <allania7med1@gmail.com>
2020-12-30 13:21:39 +0100

    store work

---
+++ store/index.js
@@ -0,0 +1,16 @@
+export const strict = false;
+
+export const state = () => ({
+  count: 0
+});
+export const getters = {
+  count2x: state => {
+    return 2 * state.count;
+  }
+};
+
+export const mutations = {
+  increment(state, n) {
+    state.count += n;
+  }
+};
